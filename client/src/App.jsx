5084f72504e61e534a386f5e7d5d6b3c9c59ab28
nitin562 <nitindbas8800@gmail.com>
2024-03-31 06:31:42 +0530

    Touch Enability

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,22 @@
 import { DndProvider } from 'react-dnd'
-
+import {HTML5Backend} from "react-dnd-html5-backend"
 import { TouchBackend } from 'react-dnd-touch-backend';
 import './App.css'
 import Home from './Components/Home'
+import useSelection from 'antd/es/table/hooks/useSelection';
+import { useEffect, useState } from 'react';
 
 function App() {
-
-
+  const [TouchStart, setTouchStart] = useState(false)
+  useEffect(()=>{
+    const onTouchStart=()=>setTouchStart(true)
+    window.addEventListener("touchstart",onTouchStart)
+    return()=>{
+      window.removeEventListener("touchstart",onTouchStart)
+    }
+  })
   return (
-    <DndProvider backend={TouchBackend}>
+    <DndProvider backend={TouchStart?TouchBackend:HTML5Backend}>
     <Home/>
     </DndProvider>
   )
